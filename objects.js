df6346648cc259e02d0a80ac94f4b79e1db2d9ef
Lige <lige.williamson@gmail.com>
2018-05-31 12:51:45 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var myBloodyValentine = 'My Bloody Valentine' 
+var playlist = {
+  Slowdive: 'Alison'
+  myBloodyValentine: 'Sometimes'
+}
+
+// function updatePlaylist() {
+//   playlist.['Phil Ochs'] = ['Here\'s to the State of Mississippi']
+//   return playlist
+// }
+//
+// function removeFromPlaylist() {
+//   delete playlist.['Slowdive']
+// }
